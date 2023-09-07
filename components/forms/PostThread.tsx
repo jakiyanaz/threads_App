100cbd02f85b30e02897b6a4dc1600fb51befaa2
Jakiya Naz <nazjakiya1234@gmail.com>
2023-09-07 15:37:16 +0530

    deploy1 commit

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -3,6 +3,7 @@
 import * as z from "zod";
 import { useForm } from 'react-hook-form';
 import { Button } from '@/components/ui/button';
+import { useOrganization } from "@clerk/nextjs";
 import {
     Form,
     FormControl,
@@ -35,6 +36,7 @@ interface props {
 function PostThread({ userId }: {userId: string}) {
     const router = useRouter();
     const pathname = usePathname();
+    const { organization } = useOrganization();
 
     const form = useForm({
         resolver: zodResolver(ThreadValidation),
@@ -48,7 +50,7 @@ function PostThread({ userId }: {userId: string}) {
         await createThread({
           text: values.thread,
           author: userId,
-          communityId: null,
+          communityId: organization ? organization.id : null,
           path: pathname,
         });
     
