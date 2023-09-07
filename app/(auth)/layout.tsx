3b0104ba9590b993b91cddef0dda050b4807adf2
Jakiya Naz <nazjakiya1234@gmail.com>
2023-09-07 10:48:09 +0530

    Initial commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,29 @@
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+
+import '../globals.css';
+
+export const metadata = {
+    title: "Auth",
+    description: "A next.js 13 Meta Threads Application",
+}
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({
+    children,
+  }: {
+    children: React.ReactNode;
+  }) {
+    return (
+      <ClerkProvider>
+        <html lang='en'>
+          <body className={`${inter.className} bg-dark-1`}>
+            <div className="w-full flex justify-center items-center">
+              {children}
+            </div>
+          </body>
+        </html>
+      </ClerkProvider>
+    );
+  }
